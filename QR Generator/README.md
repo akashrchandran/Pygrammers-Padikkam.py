5a722852d5999c4ca399f27c29b923e179efd3b9
Akash R Chandran <chandranrakash@gmail.com>
2023-01-31 12:30:09 +0530

    added print qr code in terminal

--- QR Generator/README.md
+++ QR Generator/README.md
@@ -1,5 +1,10 @@
 # QR Code generator
 A simple QR code generator made using python
+
+> Install dependency using command
+```
+pip install qrcode
+```
 >Run using this command
 ```
 python qr_gen.py
--- QR Generator/qr_gen.py
+++ QR Generator/qr_gen.py
@@ -1,11 +1,15 @@
 import os
 import qrcode
 
+qr = qrcode.QRCode()
 os.makedirs('qrcodes', exist_ok=True)
 print("QR Code Generator", end="\n\n")
 print("Enter any data and it will generate QR corresponding to that data!")
 data = input("Enter any data: ")
-img = qrcode.make(data)
-rand_file_name = f"qrcodes/{data[:5]}.png"
-img.save(rand_file_name)
-print("QR code saved at: ", os.path.abspath(rand_file_name))
+qr.add_data(data)
+file_name = f"qrcodes/{data[:5]}.png"
+img = qr.make_image()
+img.save(file_name)
+print("Generated QR code:")
+qr.print_ascii()
+print("QR code saved at: ", os.path.abspath(file_name))
